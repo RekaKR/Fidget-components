f4da34ca180160bcb4052884a613215d900717ae
RekaKR <RekaKR@users.noreply.github.com>
2021-01-26 11:26:36 +0100

    Update BoxButton.jsx & app.scss

--- fidget-c/src/components/boxButton.jsx
+++ fidget-c/src/components/boxButton.jsx
@@ -2,15 +2,15 @@ import React, { useState } from 'react';
 import '../style/app.css';
 
 function BoxButton() {
-  const [backgroundColor, setButtonColor] = useState('#BADA55');
+  const [color, setButtonColor] = useState('#2fadad');
 
   return (
     <div className="box-button">
-      <button style={{ backgroundColor }} onClick={() => { setButtonColor('#' + Math.random().toString(16).substr(-6)) }}>első</button>
-      <button style={{ backgroundColor }} onClick={() => { setButtonColor('#' + Math.random().toString(16).substr(-6)) }}>második</button>
-      <button style={{ backgroundColor }} onClick={() => { setButtonColor('#' + Math.random().toString(16).substr(-6)) }}>harmadik</button>
-      <button style={{ backgroundColor }} onClick={() => { setButtonColor('#' + Math.random().toString(16).substr(-6)) }}>negyedik</button>
-      <button style={{ backgroundColor }} onClick={() => { setButtonColor('#' + Math.random().toString(16).substr(-6)) }}>ötödik</button>
+      <button style={{ color }} onClick={() => { setButtonColor('#' + Math.random().toString(16).substr(-6)) }}>első</button>
+      <button style={{ color }} onClick={() => { setButtonColor('#' + Math.random().toString(16).substr(-6)) }}>második</button>
+      <button style={{ color }} onClick={() => { setButtonColor('#' + Math.random().toString(16).substr(-6)) }}>harmadik</button>
+      <button style={{ color }} onClick={() => { setButtonColor('#' + Math.random().toString(16).substr(-6)) }}>negyedik</button>
+      <button style={{ color }} onClick={() => { setButtonColor('#' + Math.random().toString(16).substr(-6)) }}>ötödik</button>
     </div>
   );
 }
