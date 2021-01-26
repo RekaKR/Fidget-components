32fbdc18d17b36fbbd7bbcc0848fb5f1acbb4524
RekaKR <RekaKR@users.noreply.github.com>
2021-01-26 11:06:29 +0100

    Eit boxcounter.jsx & boxbutton.jsx & style

--- fidget-c/src/components/BoxCounter.jsx
+++ fidget-c/src/components/BoxCounter.jsx
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../style/app.css';
 
 function BoxCounter() {
   const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    if (counter > 10 || counter < -10) {
+      setCounter("OK, THAT'S IT!")
+    }
+  }, [counter]);
+
+  useEffect(() => {
+    if (isNaN(counter) && counter !== "OK, THAT'S IT!") {
+      setCounter(0)
+    }
+  }, [counter]);
 
   return (
     <div className="box-counter">
--- fidget-c/src/components/boxButton.jsx
+++ fidget-c/src/components/boxButton.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../style/app.css';
 
 function BoxButton() {
+  const [backgroundColor, setButtonColor] = useState('#BADA55');
+
   return (
     <div className="box-button">
-      <button>első</button>
-      <button>második</button>
-      <button>harmadik</button>
-      <button>negyedik</button>
-      <button>ötödik</button>
+      <button style={{ backgroundColor }} onClick={() => { setButtonColor('#' + Math.random().toString(16).substr(-6)) }}>első</button>
+      <button style={{ backgroundColor }} onClick={() => { setButtonColor('#' + Math.random().toString(16).substr(-6)) }}>második</button>
+      <button style={{ backgroundColor }} onClick={() => { setButtonColor('#' + Math.random().toString(16).substr(-6)) }}>harmadik</button>
+      <button style={{ backgroundColor }} onClick={() => { setButtonColor('#' + Math.random().toString(16).substr(-6)) }}>negyedik</button>
+      <button style={{ backgroundColor }} onClick={() => { setButtonColor('#' + Math.random().toString(16).substr(-6)) }}>ötödik</button>
     </div>
   );
 }
